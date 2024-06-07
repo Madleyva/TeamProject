bc68ec76e8c9df8593d27296ca369cead78ebea1
Madleyva <160652565+Madleyva@users.noreply.github.com>
2024-06-07 11:19:55 -0700

    NewThings

--- NewStartMaddy.js
+++ NewStartMaddy.js
@@ -1,8 +1,11 @@
 let clocks = [];
-
+let timeSelect;
+let colorButton;
+let isYellow = true; // State variable to track the current background color
 
 function setup() {
-    createCanvas(800, 1200);
+    let canvas = createCanvas(800, 1200);
+    canvas.parent('sketch-container'); // Ensures the canvas is inside the 'sketch-container' div
     textAlign(CENTER, CENTER);
     noSmooth();
 
@@ -18,32 +21,38 @@ function setup() {
         'Beijing, China-CST' //+8
     ];
 
-    let timeOffset = [0,2,4,6,8,10,12,14,16];
+    let timeOffset = [0, 2, 4, 6, 8, 10, 12, 14, 16];
 
-    // Adjust the positions for your specific layout
+    // Creating the select dropdown for timezones
     timeSelect = createSelect();
     timeSelect.position(300, 750);
+    timeSelect.parent('sketch-container');
 
-    for(let i = 0; i < names.length; i++){
+    for (let i = 0; i < names.length; i++) {
         timeSelect.option(names[i]);
     }
-    
+
+    // Add a callback for when the dropdown value changes
     timeSelect.changed(timeSelection);
 
-    let i = 0;
-    let x = 400
-    let y = 450
-    clocks.push(new Clock(x, y, names[0],timeOffset[0]));
+    let x = 400;
+    let y = 450;
+    clocks.push(new Clock(x, y, names[0], timeOffset[0]));
 
-    // drop down menu
+    // Set initial background color
+    changeBackGroundColor('#f5f542'); // yellow
+
+    // Create a button to change the background color
+    colorButton = createButton('Change Color');
+    colorButton.position(width/2, 800);
+    colorButton.class('color-button');
+    colorButton.mousePressed(toggleBackgroundColor); // Change to turquoise on button press
 }
 
 function draw() {
     background('#E24672');
-    //Text 
     noStroke();
     textSize(55);
-
     fill('white');
     text('World Time Zones', width / 2, 150);
 
@@ -51,15 +60,28 @@ function draw() {
         clock.update();
         clock.display();
     }
-
-    // Group Project - Eli Fenix, Maddy Leyva, Erin Lee.
 }
 
-function timeSelection(){
+function timeSelection() {
     let nameSelected = timeSelect.selected();
-    //let timeOff = timeSelect.value();
     clocks[0].name = nameSelected;
-    //clocks[0].timeOffset = parseInt(timeOff);
+
+    // Change background color when timezone is selected
+    changeBackGroundColor('#5ae8e6'); // turquoise
+}
+
+//parameter.... this took so long and im still not happy with it -maddy
+function changeBackGroundColor(hexColor) {
+    document.body.style.backgroundColor = hexColor;
+}
+
+function toggleBackgroundColor() {
+    if (isYellow) {
+        changeBackGroundColor('#5ae8e6'); // Change to turquoise
+    } else {
+        changeBackGroundColor('#f5f542'); // Change to yellow
+    }
+    isYellow = !isYellow; // Toggle the state
 }
 
 class Clock {
@@ -74,13 +96,11 @@ class Clock {
     }
 
     display() {
-        // The ellipses
         fill(51);
         noStroke();
         ellipse(this.x, this.y, 400, 400);
 
-
-        //Code for the minute ticks 
+        // Code for the minute ticks 
         for (let i = 0; i < 60; i++) {
             let angle = map(i, 0, 60, 0, TWO_PI) - HALF_PI;
             let x1 = this.x + 2 * cos(angle) * 92;
@@ -92,56 +112,41 @@ class Clock {
             line(x1, y1, x2, y2);
         }
 
-
-        //names of each time zone 
+        //Name coordinates and font sizes
         noStroke();
         fill('white');
         textSize(18);
         text(this.name, this.x, this.y - 100);
 
-        //To get the current time, Have to Update for clocks 
-        let timeSet = hour()
+        //To get the current time
         let s = second();
         let m = minute();
         let h = hour();
-
-        //Adjust hours based on time zone offsets
-        /*if(this.name === 'Los Angeles, USA-PST') h = (h - 8+24)%24;*/
-
-        //I commented out the code above so it will be the current time on the computer, you can use it if you want or delete it when you code the correct times :0
-
-
-
 
         //Formatted digital clock
         let formattedTime = nf(h, 2) + ':' + nf(m, 2) + ':' + nf(s, 2);
         textSize(24);
         text(formattedTime, this.x, this.y + 120);
 
-
-        //The calculated angles for the clock hands 
-        //The HALF_PI is creating lines that are angled 
-        //The TWO_PI is creating a single line that is in the center 
+        //Calculated angles for the clock hands 
         let secondAngle = map(s, 0, 60, 0, TWO_PI) - HALF_PI;
         let minuteAngle = map(m, 0, 60, 0, TWO_PI) - HALF_PI;
         let hourAngle = map(h % 12, 0, 12, 0, TWO_PI) - HALF_PI;
 
-
-        //The second hand 
-        stroke('red'); //color of the pointer
+        // The second hand 
+        stroke('red');
         strokeWeight(2);
         line(this.x, this.y, this.x + cos(secondAngle) * 120, this.y + sin(secondAngle) * 120);
 
-        //The minute hand 
-        stroke(255); //color of the pointer
+        // The minute hand 
+        stroke(255);
         strokeWeight(3.75);
         line(this.x, this.y, this.x + cos(minuteAngle) * 140, this.y + sin(minuteAngle) * 140);
 
-        //The hour hand 
-        stroke(255); //the color of the pointer
+        // The hour hand 
+        stroke(255);
         strokeWeight(6.5);
         line(this.x, this.y, this.x + cos(hourAngle) * 105, this.y + sin(hourAngle) * 100);
-
     }
 }
-
+// Eli Fenix, Maddy Leyva, Erin Lee
